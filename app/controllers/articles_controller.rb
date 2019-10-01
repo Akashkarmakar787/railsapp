5622d5ca420c4023d0cc22faa14db0680c8e0abe
Akash Karmakar <akashkarmakar787@gmail.com>
2019-09-30 23:57:17 -0700

    user can add his own article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,7 +26,7 @@ end
  	
  	#render plain: params[:article].inspect
  	@article =Article.new(article_params)
- 	@article.user=User.first
+ 	@article.user= current_user
  	if @article.save
  		flash[:notice]="Article was successfully created"
  		redirect_to article_path(@article)
