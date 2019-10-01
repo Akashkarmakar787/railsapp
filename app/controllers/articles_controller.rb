94faae5717ecc9bc11f841fa827852617974035c
Akash Karmakar <akashkarmakar787@gmail.com>
2019-09-30 23:54:37 -0700

    authentication added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,14 @@
 class ArticlesController < ApplicationController
 	#before_action :set_article, only: [:edit, :update, :show, :destroy]
 	#before_action :set_article, only: [:edit, :update, :show, :destroy]
+before_action :set_article, only: [:edit, :update, :show, :destroy]
+before_action :require_user, except: [:index, :show]
+before_action :require_same_user, only: [:edit, :update, :destory]
 def edit
-	@article= Article.find(params[:id])
+	#@article= Article.find(params[:id])
 end
 def update
-	@article=Article.find(params[:id]);
+	#@article=Article.find(params[:id]);
 	if @article.update(article_params)
 		flash[:notice]="Article was successfully updated"
 		redirect_to article_path(@article)
@@ -34,10 +37,10 @@ end
  	
  end
  def show
- 	@article= Article.find(params[:id]);
+ 	#@article= Article.find(params[:id]);
  end
  def destroy
- 	@article=Article.find(params[:id]);
+ 	#@article=Article.find(params[:id]);
  	@article.destroy
  	flash[:notice]="Article was successfully deleted"
  	redirect_to articles_path
@@ -46,4 +49,14 @@ end
  def article_params
  	params.require(:article).permit(:title, :description)
  end
+ def set_article
+ 	@article= Article.find(params[:id]);
+ end
+ def require_same_user
+ 	if current_user != @article.user
+ 		flash[:danger]="you can only delete or edit your own articles"
+ 		redirect_to root_path
+ 	end
+ end
+
 end
